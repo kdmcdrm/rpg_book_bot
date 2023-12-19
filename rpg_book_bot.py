83a8f6f3eeb7e865e800302098e86c36b1293e82
Kevin MacDermid <kevin.macdermid@gmail.com>
2023-12-18 21:51:54 -0400

    Cleaned up and improved prompting.

--- rpg_book_bot.py
+++ rpg_book_bot.py
@@ -12,13 +12,21 @@ logger = logging.getLogger(__name__)
 _ = load_dotenv()
 
 # Streamlit app
-st.title("DM Genius")
+st.set_page_config(page_title="D&D Rulebot")
+st.title("D&D Rulebot")
+st.markdown("He knows the rules better than you do.")
+# ToDo: Have GPT generate a tagline on the fly
+
+ROLE_MAP = {
+    "user": "You",
+    "assistant": "DM"
+}
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
         {"role": "system",
          "content": "You are the smartest Dungeon Master in the land, you make fun of the weak minded fools while "
-                    "answering their questions."}
+                    "answering their questions accurately."}
     ]
 
 if "retriever" not in st.session_state:
@@ -40,32 +48,41 @@ def generate_response(question: str,
     prompt_template = """
 Use the following pieces of retrieved context to answer the following question. If you don't know the answer, 
 mock the questioner to hide your shame. Keep the answer concise if you do know it, but make it clear that the
-questioner should have known it themselves.
+questioner should have known it themselves. Refer to the context as The Rulebook, always capitalize it. 
     Question: {question} 
     Context: {context}
 """
     prompt_str = prompt_template.format(question=question, context=get_and_format_context(question, retriever_))
     prompt_msg = {"role": "user", "content": prompt_str}
-    response = openai.ChatCompletion.create(
+    res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",  # Use the ChatGPT model
         messages=chat_history[:-1] + [prompt_msg],  # replace last message with prompted message
         temperature=0.2
     )
-    return response.choices[0].message["content"].strip()
+    return res.choices[0].message["content"].strip()
 
 
-user_input = st.text_input("You:", "")
-if user_input:
-    # Append user message to chat history
+# Draw previous history to the screen
+for message in st.session_state.chat_history:
+    if message["role"] == "system":
+        continue
+    with st.chat_message(ROLE_MAP[message["role"]]):
+        st.markdown(message["content"])
+
+if user_input := st.chat_input("Ask if you must..."):
+    with st.chat_message(ROLE_MAP["user"]):
+        st.markdown(user_input)
+
+    # Add request to chat history
     st.session_state.chat_history.append({"role": "user", "content": user_input})
 
-    st.text("DM:")
     # Use chat history to generate AI response
-    ai_response = generate_response(user_input,
-                                    st.session_state.retriever,
-                                    st.session_state.chat_history)
+    response = generate_response(user_input,
+                                 st.session_state.retriever,
+                                 st.session_state.chat_history,
+                                 )
 
+    with st.chat_message(ROLE_MAP["assistant"]):
+        st.markdown(response)
     # Append AI response to chat history
-    st.session_state.chat_history.append({"role": "assistant", "content": ai_response})
-
-    st.write(ai_response)
+    st.session_state.chat_history.append({"role": "assistant", "content": response})
