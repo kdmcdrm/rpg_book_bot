dc75397a9958591646908c5133ea5d101bf9848a
Kevin MacDermid <kevin.macdermid@gmail.com>
2023-12-24 16:20:59 -0400

    Adds page number to the output.

--- rpg_book_bot.py
+++ rpg_book_bot.py
@@ -3,7 +3,7 @@ Simple AI RPG Book Rulebot
 
 ToDo:
  - Add page number handling
- - Add ability to have multiple books for single ruleset (OSE + carcass crawlers)
+ - Add ability to have multiple books for single ruleset (OSE + carcass crawlers) - Library class + Additional details in book_map
  - Modify to Discord bot
  - Improvement: Get LLM to process question to include any needed context from previous conversation, currently only
     the immediate question goes to the bot.
@@ -29,9 +29,8 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 st.set_page_config(page_title="Rules Master 9000")
 st.title("Rules Master 9000")
 st.markdown(
-    "I am the Rules Master 9000. Ask me your D&D rules questions and bask in my superior knowledge feeble human.")
+    "I am the Rules Master 9000. Ask me your D&D rules questions and bask in my superior knowledge.")
 
-# ToDo: These should be in the session state
 ROLE_MAP = {
     "user": "You",
     "assistant": "RM9000"
@@ -40,15 +39,12 @@ SYS_MSG = (
     "You are the smartest Dungeon Master in the land, you often make fun of the weak minded fools while answering"
     " their questions accurately.")
 
-QUESTION_TEMPLATE = """Use the following pieces of the Rulebook to answer the question below. 
-Keep the answer concise know the answer, dismiss the question as pointless if you don't. Feel free to demean the
-asker for asking such simple questions that everyone should already know the answers to. Do not use the same insults
-each time you answer a question.
-    Question: {question} 
-    Context: {context}
-"""
+QUESTION_TEMPLATE = """Use the following pairs of page number and Rulebook passage to answer the question below. 
+Provide the page number and keep the answer concise if you know the answer, dismiss the question as pointless if you 
+don't. Feel free to demean the asker for asking such simple questions that everyone should already know the answers 
+to. Do not use the same insults each time you answer a question. Question: {question} Context: {context}"""
 with open("./books/book_map.json", "r") as fh:
-    BOOK_MAP = json.load(fh)
+    st.session_state["book_map"] = json.load(fh)
 
 if "agent" not in st.session_state:
     st.session_state.agent = OpenAIRagAgent(os.environ["OPENAI_MODEL_NAME"],
@@ -56,26 +52,24 @@ if "agent" not in st.session_state:
                                             SYS_MSG,
                                             QUESTION_TEMPLATE)
 
-
 with st.sidebar:
     st.markdown("# Configuration")
     rulebook_name = st.selectbox("Select Ruleset",
-                                 BOOK_MAP.keys(),
+                                 st.session_state["book_map"].keys(),
                                  index=0)
-    st.selectbox("Select Personality", options=["Arrogant"], disabled=True)
-
+    st.selectbox("Select Personality",
+                 options=["Arrogant", "Mean-Spirited", "Smarter Than You", "This doesn't do anything lol"])
 
 # Sets the initial rulebook
 if "rulebook" not in st.session_state:
-    index_path = Path("./books") / BOOK_MAP[rulebook_name]["index_dir"]
-    st.session_state["rulebook"] = FAISS.load_local(index_path, OpenAIEmbeddings()).as_retriever(k=1)
+    index_path = Path("./books") / st.session_state["book_map"][rulebook_name]["index_dir"]
+    st.session_state["rulebook"] = FAISS.load_local(index_path, OpenAIEmbeddings()).as_retriever(search_kwargs={"k": 3})
     st.session_state["rulebook_name"] = rulebook_name
 
 if st.session_state["rulebook_name"] != rulebook_name:
-    index_path = Path("./books") / BOOK_MAP[rulebook_name]["index_dir"]
-    st.session_state["rulebook"] = FAISS.load_local(index_path, OpenAIEmbeddings()).as_retriever(k=1)
+    index_path = Path("./books") / st.session_state["book_map"][rulebook_name]["index_dir"]
+    st.session_state["rulebook"] = FAISS.load_local(index_path, OpenAIEmbeddings()).as_retriever(search_kwargs={"k": 3})
     st.session_state["rulebook_name"] = rulebook_name
-
 
 # Draw previous history to the screen
 for message in st.session_state.agent.history:
@@ -90,8 +84,8 @@ if user_input := st.chat_input("Ask if you must..."):
         st.markdown(user_input)
 
     # Use Agent to generate response
-    # ToDo: Add page number here
-    context = [x.page_content for x in st.session_state["rulebook"].get_relevant_documents(user_input)]
+    context = [f'pg {doc.metadata["page"]}: {doc.page_content}'
+               for doc in st.session_state["rulebook"].get_relevant_documents(user_input)]
     response = st.session_state.agent(user_input, context)
 
     with st.chat_message(ROLE_MAP["assistant"]):
