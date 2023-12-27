3dcf2e70208bf34cdec2c4d0a38893086abc2a77
Kevin MacDermid <kevin.macdermid@gmail.com>
2023-12-27 17:46:08 -0400

    WIP Switch book map to yaml. Not supported by main bot yet.

--- process_rulebooks.py
+++ process_rulebooks.py
@@ -1,4 +1,3 @@
-import os
 
 from dotenv import load_dotenv
 from langchain.document_loaders import PyPDFLoader
@@ -8,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from pathlib import Path
 import logging
+import yaml
 
 
 logger = logging.getLogger(__name__)
@@ -19,21 +19,25 @@ def process_rulebooks():
     Processes a rulebook to create a vector store for Retrieval
     Augmented Generation.
     """
-    logger.info("---  Read Rulebook --- ")
-    with open("./books/book_map.json", "r") as fh:
-        books = json.load(fh)
-    for name, paths in books.items():
-        rel_path = Path("./books") / paths["raw_filename"]
-        logger.info(f"Loading rulebook from {str(rel_path)}")
-        loader = PyPDFLoader(str(rel_path))
-        docs = loader.load()
+    logger.info("---  Process Rulebooks --- ")
+    with open("./books/book_map.yaml", "r") as fh:
+        rulesets = yaml.safe_load(fh)
+
+    for ruleset, ruleset_rec in rulesets.items():
+        logger.info(f"Starting ruleset {ruleset}")
+        docs = []
+        for book in ruleset_rec["books"]:
+            rel_path = Path("./books") / book["filename"]
+            logger.info(f"Loading book {book['book_name']} from {str(rel_path)}")
+            loader = PyPDFLoader(str(rel_path))
+            docs += loader.load()
 
         splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = splitter.split_documents(docs)
 
         logger.info("Creating vector store")
         vector_store = FAISS.from_documents(splits, OpenAIEmbeddings())
-        out_path = Path("./books") / paths["index_dir"]
+        out_path = Path("./books") / ruleset_rec["index_dir"]
         logger.info(f"Saving vectorstore to {out_path}")
         vector_store.save_local(out_path)
 
